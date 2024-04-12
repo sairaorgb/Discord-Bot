64472469c886b7f086269fdf20521f91c3f7de13
Whackerfin <135827600+Whackerfin@users.noreply.github.com>
2024-04-12 23:02:12 +0530

    Changed the text extraction code

--- text_extraction/main.py
+++ text_extraction/main.py
@@ -1,8 +1,7 @@
 import os
 import google.generativeai as genai
 import textwrap
-import pprint
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))#Replace with API KEY
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 model = genai.GenerativeModel('gemini-pro')
 def to_markdown(text):
   
@@ -13,14 +12,13 @@ def to_markdown(text):
   text = text.replace('•', '  *')
   return f"`\n{textwrap.indent(text, '> ', predicate=lambda _: True)}\n`"
 extracted_text=""""""#Replace with the text from which you want to extract data
-prompt="The provided text is extracted from an image of the job listing or experience.From the provided text extract out {}.Only provide from the text given.If no such data is available for the given prompt then just output empty string."
-required_fields=["The test questions","Interview round questions","managerial round question","hr round questions","overall experience","suggestions"]#Add in the fields you need from the text
+prompt="The provided text is extracted from an image of the job listing.From the provided text extract out {}.Only provide from the text given."
+required_fields=["Skills required","Vacancies","Location"]#Add in the fields you need from the text
 output=[]#Stores all the results
 for field in required_fields:
     final_prompt=prompt.format(field)
     response=model.generate_content([extracted_text,final_prompt],stream=True)
     response.resolve()
-    output.append((response.text))
+    output.append(to_markdown(response.text))
 
-pprint.pprint(output)
     
